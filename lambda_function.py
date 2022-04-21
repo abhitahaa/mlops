734929db4799bb25e979021f28c106b802a540f6
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-04-21 15:51:09 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,5 @@ def lambda_handler(event, context):
     
     return {
         'statusCode' : 200,
-        #'body' : json.dumps('Hello from Lambda!'),
-        'body':json.dumps(event['headers']['X-Forwarded-For'])
-    }
+        'body' : json.dumps('Hello from Lambda!')
+            }
