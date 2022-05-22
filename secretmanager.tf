b71691be716182752f006debf1d76c67490d8f0f
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-05-22 17:24:55 -0400

    Update secretmanager.tf

--- secretmanager.tf
+++ secretmanager.tf
@@ -47,6 +47,6 @@ EOF
  
 # After importing the secrets storing into Locals
  
-locals {
-  db_creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
-}
+# locals {
+#   db_creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
+# }
